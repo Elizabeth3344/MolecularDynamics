914c79d316f78820775f919705a34afb5ddc41d2
Elizabeth3344 <lizka21848@gmai.com>
2025-04-17 17:37:46 +0300

    MOF5.data added

--- Tasks/MOF-5/cif2lammps.py
+++ Tasks/MOF-5/cif2lammps.py
@@ -1,16 +1,16 @@
-# from ase.io import read, write
-# mof5 = read("MOF5.cif")
-# write("MOF5.data", mof5, format="lammps-data")
+from ase.io import read, write
+mof5 = read("MOF5.cif")
+write("MOF5.data", mof5, format="lammps-data")
 
-from pymatgen.io.cif import CifParser
-from pymatgen.io.lammps.data import LammpsData
+# from pymatgen.io.cif import CifParser
+# from pymatgen.io.lammps.data import LammpsData
 
-# Загрузка CIF файла
-parser = CifParser('MOF-5.cif')
-structure = parser.get_structures()[0]
+# # Загрузка CIF файла
+# parser = CifParser('MOF-5.cif')
+# structure = parser.get_structures()[0]
 
-# Создание объекта LammpsData
-lammps_data = LammpsData.from_structure(structure, atom_style='full')
+# # Создание объекта LammpsData
+# lammps_data = LammpsData.from_structure(structure, atom_style='full')
 
-# Запись в файл
-lammps_data.write_file('MOF-5.data')
+# # Запись в файл
+# lammps_data.write_file('MOF-5.data')
