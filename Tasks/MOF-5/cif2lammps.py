fc1fb198b76bd0dd76adb5e2c37f3ea6b7129d2b
Elizaveta Chernysheva <lizka21848@gmail.com>
2025-04-17 16:44:39 +0300

    cif2lammps

---
+++ Tasks/MOF-5/cif2lammps.py
@@ -0,0 +1,16 @@
+# from ase.io import read, write
+# mof5 = read("MOF5.cif")
+# write("MOF5.data", mof5, format="lammps-data")
+
+from pymatgen.io.cif import CifParser
+from pymatgen.io.lammps.data import LammpsData
+
+# Загрузка CIF файла
+parser = CifParser('MOF-5.cif')
+structure = parser.get_structures()[0]
+
+# Создание объекта LammpsData
+lammps_data = LammpsData.from_structure(structure, atom_style='full')
+
+# Запись в файл
+lammps_data.write_file('MOF-5.data')
